a3546dd8d5ea872f8b37d5f59c6c3dec382a3e7e
Leilani Reich <reichld@hawaii.edu>
2022-11-16 11:54:25 -1000

    Additions to AgPestUse cleaning

--- preprocessing_work/AgPestUse_fixes.py
+++ preprocessing_work/AgPestUse_fixes.py
@@ -20,7 +20,7 @@ neonics_in_data[0] = [i.upper() for i in neonicotinoids if i.upper() in lowEst_d
 neonics_in_data[1] = [i.upper() for i in neonicotinoids if i.upper() in highEst_df['Compound'].unique()]
 
 # Gets rid of rows that address pesticides other than neonicotinoids
-LowEst_df_filtered = lowEst_df[lowEst_df['Compound'].isin(neonics_in_data[0])]
+lowEst_df_filtered = lowEst_df[lowEst_df['Compound'].isin(neonics_in_data[0])]
 highEst_df_filtered = highEst_df[highEst_df['Compound'].isin(neonics_in_data[1])]
 
 # Checking how lengths of datasets change after filtering out non-neonicotinoids
@@ -46,17 +46,37 @@ highEst_df_filtered = highEst_df[highEst_df['Compound'].isin(neonics_in_data[1])
 
 
 # Only kg units were used, so I dropped the units column
-LowEst_df_filtered.drop('Units', axis=1, inplace=True)
-highEst_df_filtered.drop('Units', axis=1, inplace=True)
+lowEst_df_filtered = lowEst_df_filtered.drop('Units', axis=1)
+highEst_df_filtered = highEst_df_filtered.drop('Units', axis=1)
 
 # State_FIPS_code is not needed since we have the State column
-LowEst_df_filtered.drop('State_FIPS_code', axis=1, inplace=True)
-highEst_df_filtered.drop('State_FIPS_code', axis=1, inplace=True)
+lowEst_df_filtered = lowEst_df_filtered.drop('State_FIPS_code', axis=1)
+highEst_df_filtered = highEst_df_filtered.drop('State_FIPS_code', axis=1)
 
-# Group by state and sort by year?
-# -----
-# -----
+
+# Sorting by year:
+
+lowEst_df_filtered.sort_values(by=['Year'], inplace=True)
+highEst_df_filtered.sort_values(by=['Year'], inplace=True)
+
+# RemovingNaN (replacing with -1 for now)
+
+# print(lowEst_df_filtered.isna().sum())
+# print(highEst_df_filtered.isna().sum())
+
+lowEst_df_filtered.fillna(value=-1, inplace=True)
+highEst_df_filtered.fillna(value=-1, inplace=True)
+
+
+# Reset indices in datasets
+lowEst_df_filtered.reset_index(drop=True, inplace=True)
+highEst_df_filtered.reset_index(drop=True, inplace=True)
+
+
+# Printing cleaned datasets
+#print(lowEst_df_filtered.head)
+#print(highEst_df_filtered.head)
 
 # Saving cleaned data
-LowEst_df_filtered.to_csv('../preprocessed_bee_data/LowEst_AgPestUse_clean.csv')
+lowEst_df_filtered.to_csv('../preprocessed_bee_data/LowEst_AgPestUse_clean.csv')
 highEst_df_filtered.to_csv('../preprocessed_bee_data/highEst_AgPestUse_clean.csv')
